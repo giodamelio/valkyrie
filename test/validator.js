1ed92c2e832f8c29af31f5adebb94b6572e53269
Gio d'Amelio <giodamelio@gmail.com>
2015-03-22 18:29:42 -0700

    Add tests for username validation

---
+++ test/validator.js
@@ -0,0 +1,37 @@
+var validator = require("../src/validator");
+
+describe("Validators", function() {
+    describe("Username", function() {
+        it("Valid", function(done) {
+            validator.username("giodamelio")(function(err) {
+                if (err) return done(err);
+                done();
+            });
+        });
+        it("Valid (Max Length)", function(done) {
+            validator.username("ThisNameIs24LettersLongA")(function(err) {
+                if (err) return done(err);
+                done();
+            });
+        });
+        it("Valid (Min Length)", function(done) {
+            validator.username("6Chars")(function(err) {
+                if (err) return done(err);
+                done();
+            });
+        });
+        it("Not a String", function(done) {
+            validator.username(42)(function(err) {
+                if (err) return done();
+                done("Username is not not a string");
+            });
+        });
+        it("Non Alphanumerical", function(done) {
+            validator.username("No_Symbols-Allowed")(function(err) {
+                if (err) return done();
+                done("Username is not non alphanumerical");
+            });
+        });
+    });
+});
+
